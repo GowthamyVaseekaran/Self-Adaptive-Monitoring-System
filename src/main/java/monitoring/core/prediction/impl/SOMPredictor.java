2abc3ab07d4ddb34c63ea5d9a19d1a5aaf551a56
gowthamy.95 <vaseekarangowthamy@gmail.com>
2019-03-03 12:54:39 +0530

    change logger

--- src/main/java/monitoring/core/prediction/impl/SOMPredictor.java
+++ src/main/java/monitoring/core/prediction/impl/SOMPredictor.java
@@ -1,10 +1,13 @@
 package monitoring.core.prediction.impl;
 
 import agent.performance.metrics.impl.Metrics;
-import monitoring.core.bean.MerticObject;
+
+import monitoring.core.bean.SystemLevelMetrics;
 import monitoring.core.som.Neuron;
 import monitoring.core.som.SOM;
 import monitoring.core.som.WeightVector;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import javax.management.InstanceNotFoundException;
 import javax.management.MalformedObjectNameException;
@@ -16,11 +19,13 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+
 /**
  * This class is for the anomaly prediction.
  */
 public class SOMPredictor {
     private static final double INFINITE = Double.MAX_VALUE;
+    private static final Log logger = LogFactory.getLog(SOMPredictor.class);
 
 
     public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException, MalformedObjectNameException, InstanceNotFoundException, ReflectionException {
@@ -41,7 +46,7 @@ public class SOMPredictor {
         int secondLast = 0;
         int current = 0;
 
-        List<MerticObject> trainedData = new ArrayList<>();
+        List<SystemLevelMetrics> trainedData = new ArrayList<>();
         Metrics metricsAgent = new Metrics("17014");
 
         while (true) {
@@ -49,64 +54,66 @@ public class SOMPredictor {
             LocalDateTime now = LocalDateTime.now();
 
 
-            MerticObject merticObject = new MerticObject();
+            SystemLevelMetrics systemLevelMetrics = new SystemLevelMetrics();
             double[] metrics = new double[2];
             double cpuMetrics = metricsAgent.getSystemCPU();
-            merticObject.setCpuUsage(cpuMetrics);
-            System.out.println(cpuMetrics);
+            systemLevelMetrics.setCpuUsage(cpuMetrics);
+            logger.info(cpuMetrics);
 
             // read cpu metric from file change to 6
             metrics[0] = (Math.round((Double.parseDouble(String.valueOf(cpuMetrics)))));
             double memoryMetrics = metricsAgent.getMemoryUsage();
-            merticObject.setMemoryUsage(memoryMetrics);
-            System.out.println(memoryMetrics);
+            systemLevelMetrics.setMemoryUsage(memoryMetrics);
+            logger.info(memoryMetrics);
             double totMem = (Double.parseDouble(String.valueOf(memoryMetrics)));
 
             metrics[1] = Math.round(totMem);
+
+
             WeightVector testVector = new WeightVector(metrics);
+
             secondLast = last;
             last = current;
 
             // get prediction for input vector
             current = trainedSOM.testInput(testVector);
-            System.out.println("current status" + current);
+            logger.info("current status" + current);
 
             if (current == 1 && last == 1 && secondLast == 1) {
-                System.out.println("Tring Tring Anomaly detected");
+                logger.info("Tring Tring Anomaly detected");
                 Neuron trainedNeuron = trainedSOM.getTrainedNeuron(testVector);
                 int prediction = trainedSOM.causeInference(trainedNeuron);
 
                 // if cause inference gives CPU as metric causing anomaly
                 if (prediction == 1) {
                     cpu++;
-                    System.out.println("CPU " + cpu + " Anomaly detected cpu = " + metrics[0]);
-                }
-
-                // if cause inference gives Memory as metric causing anomaly
-                else if (prediction == 2) {
+                    logger.info("CPU " + cpu + " Anomaly detected cpu = " + metrics[0]);
+                } else if (prediction == 2) {
+                    // if cause inference gives Memory as metric causing anomaly
                     mem++;
-                    System.out.println("Mem " + mem + " Anomaly detected" + "   mem  " + metrics[1]);
+                    logger.info("Mem " + mem + " Anomaly detected" + "   mem  " + metrics[1]);
                 }
 
             } else if (current == 2 && last == 2 && secondLast == 2) {
                 mem++;
-                System.out.println("Mem " + mem + " Anomaly detected" + "   mem  " + metrics[1]);
+                logger.info ("Mem " + mem + " Anomaly detected" + "   mem  " + metrics[1]);
             } else {
+                //logger.info("normal");
                 normal++;
-                //System.out.println("Normal " + normal);
+
             }
             //Write the elements to the file
             try (FileWriter writer = new FileWriter("/home/thamy/Desktop/Self-Adaptive-Monitoring-System/HistoryData/nor.csv", true)) {
                 StringBuilder stringBuilder = new StringBuilder();
                 stringBuilder.append(now);
                 stringBuilder.append(",");
-                stringBuilder.append(merticObject.getCpuUsage());
+                stringBuilder.append(systemLevelMetrics.getCpuUsage());
                 stringBuilder.append(",");
-                stringBuilder.append(merticObject.getMemoryUsage());
+                stringBuilder.append(systemLevelMetrics.getMemoryUsage());
                 stringBuilder.append("\n");
                 writer.write(stringBuilder.toString());
             } catch (FileNotFoundException e) {
-                System.out.println(e.getMessage());
+                logger.error(e.getMessage());
             }
 
             Thread.sleep(2000L);
@@ -149,15 +156,16 @@ public class SOMPredictor {
             // read cpu metric from file change to 6
             metrics[0] = (Math.round((Double.parseDouble(data.get(6)) * 100)));
 
-            while (!memScanner.hasNextLine()) ;
+            while (!memScanner.hasNextLine());
 
             line = memScanner.nextLine();
 
             strings = line.split(",");
             data = new ArrayList<String>();
             for (String s : strings) {
-                if (s != null && s.length() > 0)
+                if (s != null && s.length() > 0) {
                     data.add(s);
+                }
             }
             double totMem = (Double.parseDouble(data.get(1)) * 100);
 
@@ -170,31 +178,29 @@ public class SOMPredictor {
 
             // get prediction for input vector
             current = trainedSOM.testInput(testVector);
-            System.out.println("current status" + current);
-            System.out.println("last" + last);
-            System.out.println("second last" + secondLast);
+            logger.info("current status" + current);
+            logger.info("last" + last);
+            logger.info("second last" + secondLast);
 
             if (current == 1 && last == 1 && secondLast == 1) {
-                System.out.println("Tring Tring Anomaly detected");
+                logger.info("Tring Tring Anomaly detected");
                 Neuron trainedNeuron = trainedSOM.getTrainedNeuron(testVector);
                 int prediction = trainedSOM.causeInference(trainedNeuron);
 
                 // if cause inference gives CPU as metric causing anomaly
                 if (prediction == 1) {
                     cpu++;
-                    System.out.println("CPU " + cpu + " Anomaly detected cpu = " + metrics[0]);
-                }
-
-                // if cause inference gives Memory as metric causing anomaly
-                else if (prediction == 2) {
+                    logger.info("CPU " + cpu + " Anomaly detected cpu = " + metrics[0]);
+                } else if (prediction == 2) {
+                    // if cause inference gives Memory as metric causing anomaly
                     mem++;
-                    System.out.println("Mem " + mem + " Anomaly detected" + "   mem  " + metrics[1]);
+                    logger.info("Mem " + mem + " Anomaly detected" + "   mem  " + metrics[1]);
 
                 }
 
             } else if (current == 2 && last == 2 && secondLast == 2) {
                 mem++;
-                System.out.println("Mem " + mem + " Anomaly detected" + "   mem  " + metrics[1]);
+                logger.info("Mem " + mem + " Anomaly detected" + "   mem  " + metrics[1]);
             } else {
                 normal++;
 
@@ -214,8 +220,6 @@ public class SOMPredictor {
         ObjectInputStream objectInputStream = new ObjectInputStream(bufferedInputStream);
         si = (SOM) objectInputStream.readObject();
         objectInputStream.close();
-        // System.out.println("iconic"+si.neurons.mapLength);
-
         return si;
     }
 
